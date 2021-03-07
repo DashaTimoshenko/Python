9c6e46bfb97bf38ae2e79035cc44782a0123a388
DashaTimoshenko <43323943+DashaTimoshenko@users.noreply.github.com>
2021-03-07 23:14:44 +0200

    Add files via upload

---
+++ Lab_1/config.py
@@ -0,0 +1,9 @@
+class Config:
+    host = "localhost"
+    database = "postgres"
+    user = "postgres"
+    password = "postgres"
+
+    file_list = ['Odata2019File.csv', 'Odata2020File.csv']
+    delimiter = ";"
+    portion = 20000
