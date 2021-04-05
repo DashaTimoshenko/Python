c0486e1b8b5cd290bae180e1fc76322eb0b69a10
DashaTimoshenko <43323943+DashaTimoshenko@users.noreply.github.com>
2021-04-05 19:46:03 +0300

    Update query_result_2.py

--- Lab_2/query_result_2.py
+++ Lab_2/query_result_2.py
@@ -8,12 +8,15 @@ conn = psycopg2.connect(
     password=Config.password)
 
 cur = conn.cursor()
-sql = """COPY (select test_tb.histPTRegName, 
-       max(case when test_tb.zno_year = 2019 then test_tb.histBall100 end) as zno_2019,
-       max(case when test_tb.zno_year = 2020 then test_tb.histBall100 end) as zno_2020
-       from test_tb 
-       where test_tb.histTestStatus = 'Зараховано'
-       group by test_tb.histPTRegName) TO STDOUT WITH CSV HEADER DELIMITER ';'"""
+sql = """COPY (select eo_view.region_name, 
+       max(case when passing_exam.zno_year = 2019 then passing_exam.Ball100 end) as zno_2019,
+       max(case when passing_exam.zno_year = 2020 then passing_exam.Ball100 end) as zno_2020
+       from passing_exam
+	   join eo_view on (passing_exam.institution_id = eo_view.institution_id)
+	   join subjects on (passing_exam.subject_id = subjects.subject_id)
+       where passing_exam.test_status = 'Зараховано' and
+	   subjects.subject_name = 'Історія України'
+	   group by eo_view.region_name) TO STDOUT WITH CSV HEADER DELIMITER ';'"""
 with open("query_res_2.csv", "w") as file:  # Файл з результатами виконання запиту
     cur.copy_expert(sql, file)
     cur.close()
