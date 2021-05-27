cd041d8356c79297ab3c09e316b967714614e7de
DashaTimoshenko <43323943+DashaTimoshenko@users.noreply.github.com>
2021-05-27 10:32:43 +0300

    Add files via upload

---
+++ Lab_3/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://qtlpdbkkfptnbn:389a1bad149cec7f77512f37d7aab73a3c87d156e4df1f007ce5125ad909b927@ec2-52-5-247-46.compute-1.amazonaws.com:5432/d6k42nuto1qosb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
